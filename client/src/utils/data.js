85677723373bd269ffb0ec42ae0a95e12a4bae5a
frangiuliano <francosebastiangiuliano@gmail.com>
2022-08-31 17:48:09 -0300

    Data creada

---
+++ client/src/utils/data.js
@@ -0,0 +1,66 @@
+[
+    {
+    name: 'Front-End',
+    description: 'Aguante el Front',
+    course: [{
+        name: 'Javascript',
+        description: 'Aguante Javascript',
+        videos: [{
+            name: 'Console.log con Midu',
+            description: 'Console.log explicado por Midu',
+            duration: '10:00min',
+            difficult: 'principiante',
+            url: 'urlCursoJavascriptMidu'
+            },
+            {
+            name: 'For con Midu',
+            description: 'For explicado por Midu',
+            duration: '15:00min',
+            difficult: 'principiante',
+            url: 'urlCursoJavascriptMidu'
+            },
+            {
+            name: 'Map con Midu',
+            description: 'Map explicado por Midu',
+            duration: '20:00min',
+            difficult: 'principiante',
+            url: 'urlCursoJavascriptMidu'
+            }
+        ],
+        image: 'urlCourse'
+    }],
+    image: 'urlSchool'
+    },
+    {
+        name: 'Back-End',
+        description: 'Aguante el Back',
+        course: [{
+            name: 'Node',
+            description: 'Aguante Node',
+            videos: [{
+                name: 'Routes con Midu',
+                description: 'Routes explicado por Midu',
+                duration: '10:00min',
+                difficult: 'principiante',
+                url: 'urlCursoNodeMidu'
+                },
+                {
+                name: 'Modelos con Midu',
+                description: 'Modelos explicado por Midu',
+                duration: '15:00min',
+                difficult: 'principiante',
+                url: 'urlCursoNodeMidu'
+                },
+                {
+                name: 'Middlewares con Midu',
+                description: 'Middlewares explicado por Midu',
+                duration: '20:00min',
+                difficult: 'principiante',
+                url: 'urlCursoNodeMidu'
+                }
+            ],
+            image: 'urlCourse'
+        }],
+        image: 'urlSchool'
+    }
+]
