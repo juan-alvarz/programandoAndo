443b92cfb0efdf91c9af53112b90fffcf5555a0e
agu6692 <figueredoagustin6692@gmail.com>
2022-08-31 17:06:30 -0300

    mod

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!!</h1>
+    <div>
+      hola
     </div>
   );
 }
