7000a25d0bfd20804f5fc537b98b6dab40b2a86a
Danzsv <danzsv94@gmail.com>
2022-08-31 20:51:33 -0500

    Ahora si Schools terminadas

---
+++ api/validators/schools.js
@@ -0,0 +1,28 @@
+const {check} = require('express-validator')
+const validateResults = require('../utils/handleValidator')
+
+const validatorCreateSchool = [
+    check("name")
+    .exists()
+    .notEmpty(),
+
+    check("description")
+    .exists()
+    .notEmpty(),
+
+    check("courses")
+    .exists()
+    .notEmpty(),
+
+    check("image")
+    .exists()
+    .notEmpty(),
+
+    (req,res,next) =>{
+        return validateResults(req,res,next)
+    }
+
+
+]
+
+module.exports = {validatorCreateSchool}
