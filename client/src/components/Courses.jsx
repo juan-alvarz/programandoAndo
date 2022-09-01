0e7772fbda8d59eac4af0cc365d62c96b9b36b72
agu6692 <figueredoagustin6692@gmail.com>
2022-08-31 21:37:52 -0300

    mostrando data en componentes de cursos

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import data from "../utils/data"
+import Video from "./Video";
 
 
 function Courses() {
@@ -27,6 +28,22 @@ function Courses() {
                          <h1>{elemento.name}</h1>
                          <p>{elemento.description}</p>
                          <p>{elemento.course[0].name}</p>
+                         <h2>VIDEOS</h2>
+                         <hr></hr>
+                         <div>
+                             {elemento.course[0].videos.map((elemento,index)=>{
+                                  return(
+                                      <div>
+                                        <Video></Video>
+                                        <h3>{elemento.name}</h3>
+                                        <p>{elemento.description}</p>
+                                      </div>
+                                  )
+                             })
+
+                             }
+
+                         </div>
                          
                      </div>
                  )
