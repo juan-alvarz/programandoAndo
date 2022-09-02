7695bf16581ebdeb933e1d97797559e364aa3a27
juan-alvarz <juanalvarez0925@gmail.com>
2022-09-01 20:17:26 -0500

    final boss

--- api/controllers/schools.js
+++ api/controllers/schools.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-const { schoolModel } = require("../models");
+const { schoolModel, courseModel } = require("../models");
 
 // ============================= GET SCHOOLS DATABASE ========================
 
@@ -10,79 +9,32 @@ const getAllSchool = async (req, res) => {
     populate: {
       path: "videos",
     },
-  });
+  }); //.populate('videos')
+  // data = JSON.parse(data)
   try {
     if (name) {
-      const find = await schoolModel.findOne({ name: name });
+      const find = await schoolModel.findOne({ name: name }).populate({
+        path: "courses",
+        populate: {
+          path: "videos",
+        },
+      });
       if (!find) {
         res.send({ msg: "School doesnt exist" });
       } else {
         res.send(find);
       }
     } else {
-      res.send(data);
+      res.json(data);
     }
   } catch (error) {
     console.log(error);
   }
 };
-=======
-const { schoolModel, courseModel } = require('../models')
-
-// ============================= GET SCHOOLS DATABASE ========================
-
-<<<<<<< HEAD
-const getAllSchool = async (req, res) => {
-    const { name } = req.query
-=======
-const getAllSchool = async (req,res) =>{
-    const {name} = req.query
->>>>>>> back
-    const data = await schoolModel.find({}).populate({
-        path: 'courses',
-        populate: {
-            path: 'videos'
-        }
-    }) //.populate('videos')
-    // data = JSON.parse(data)
-    try {
-<<<<<<< HEAD
-        if (name) {
-            const find = await schoolModel.findOne({ name: name })
-            if (!find) {
-                res.send({ msg: "School doesnt exist" })
-            } else {
-                res.send(find)
-            }
-        } else {
-            res.send(data)
-=======
-        if(name){
-            const find = await schoolModel.findOne({name:name}).populate({
-                path: 'courses',
-                populate :{
-                    path: 'videos'
-                } 
-            })
-            if(!find){
-                res.send({msg: "School doesnt exist" })         
-            }else{
-                res.send(find)
-            }
-        }else{
-            res.json(data)
->>>>>>> back
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
->>>>>>> back
 
 // ============================= GET ID SCHOOL ================================
 
 const getSchoolId = async (req, res) => {
-<<<<<<< HEAD
   const { id } = req.params;
 
   try {
@@ -121,44 +73,3 @@ const createSchool = async (req, res) => {
 };
 
 module.exports = { getAllSchool, getSchoolId, createSchool };
-=======
-    const { id } = req.params
-
-    try {
-        if (!id) {
-            res.send({ msg: "ID its necessary" })
-        } else {
-            const find = await schoolModel.findById(id)
-            if (!find) {
-                res.send({ msg: "School doesnt exist" })
-            } else {
-                res.send(find)
-            }
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-// ===========================CREATE SCHOOL ====================================
-
-const createSchool = async (req, res) => {
-    const { name, description, courses, image } = req.body
-
-    const find = await schoolModel.findOne({ name: name })
-
-    if (!find) {
-        const created = await schoolModel.create({
-            name,
-            description,
-            courses,
-            image,
-        })
-        res.send(created)
-    } else {
-        res.send({ msg: "School already exist" })
-    }
-}
-
-module.exports = { getAllSchool, getSchoolId, createSchool }
->>>>>>> back
