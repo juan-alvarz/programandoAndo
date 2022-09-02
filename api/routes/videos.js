8dac67b262b1321a72b552709a234da74efa8533
Santiago <98236588+Santy1707@users.noreply.github.com>
2022-09-02 18:48:10 -0500

    Rutas PUT-DELETE(soft)-RESTORE, y validaciones necesarias

--- api/routes/videos.js
+++ api/routes/videos.js
@@ -6,12 +6,17 @@ const {
   deleteVideo,
 } = require("../controllers/videos");
 const router = express.Router();
+const { validatorCreateVideo, validatorGetVideo } = require("../validators/videos");
+
 
 //http://localhost/track, GET, POST, DELETE, PUT
 router.get("/", getVideos);
-router.get("/:id", getVideo);
+
+router.get("/:id", validatorGetVideo, getVideo);
+
 router.delete("/:id", deleteVideo);
-router.post("/", createVideo);
+
+router.post("/", validatorCreateVideo, createVideo);
 
 module.exports = router;
 //ruta videos
