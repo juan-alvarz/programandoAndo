5d292595f149033a39410ea7c494430d549da88f
Luis Blanco <luiswblancod@gmail.com>
2022-09-08 12:49:29 -0500

    blank

--- client/src/components/Donators.jsx
+++ client/src/components/Donators.jsx
@@ -31,7 +31,9 @@ function Donators() {
 
             <div className="flex justify-center items-center my-4 mt-10">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 ">
-                Done
+                <a href="#" target="_blank">
+                  Done
+                </a>
               </button>
             </div>
           </form>
