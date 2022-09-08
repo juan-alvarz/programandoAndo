8a737cc8a7375e5782f46f52c08158d5f66265c0
juan-alvarz <juanalvarez0925@gmail.com>
2022-09-08 17:13:51 -0500

    api updated

--- api/middleware/googleSession.js
+++ api/middleware/googleSession.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { usersModel } = require("../models");
 const { handleHtppError } = require("../utils/handleError");
 const jwt_decode = require("jwt-decode");
@@ -15,21 +14,3 @@ const googleMiddleware = async (req, res, next) => {
 };
 
 module.exports = { googleMiddleware };
-=======
-const { usersModel } = require("../models");
-const { handleHtppError } = require("../utils/handleError");
-const jwt_decode = require("jwt-decode");
-
-const googleMiddleware = async (req, res, next) => {
-  const { token } = req.body;
-
-  let { email, name } = jwt_decode(token);
-  let username = email.split("@").shift();
-
-  req.body = { email, name, username };
-
-  next();
-};
-
-module.exports = { googleMiddleware };
->>>>>>> back
--- api/middleware/rol.js
+++ api/middleware/rol.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { handleHtppError } = require("../utils/handleError");
 
 const checkRol = (role) => (req, res, next) => {
@@ -23,29 +22,3 @@ const checkRol = (role) => (req, res, next) => {
 };
 
 module.exports = { checkRol };
-=======
-const { handleHtppError } = require("../utils/handleError");
-
-const checkRol = (role) => (req, res, next) => {
-  try {
-    const { user } = req;
-    const rolesByUser = user.role;
-    console.log(rolesByUser);
-
-    const checkValueRol = role.some((rolSingle) =>
-      rolesByUser.includes(rolSingle)
-    );
-
-    if (!checkValueRol) {
-      handleHtppError(res, "Usser_not_permissions", 403);
-      return;
-    }
-    next();
-  } catch (error) {
-    console.log(error);
-    handleHtppError(res, "Error_permissions", 403);
-  }
-};
-
-module.exports = { checkRol };
->>>>>>> back
