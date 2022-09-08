1cb83e24f42f3edeafcfa81bc456d6f8b5ce10ae
Luis Blanco <luiswblancod@gmail.com>
2022-09-08 12:25:03 -0500

    PA pending

--- client/src/components/panelAdmin/CoursesPA.jsx
+++ client/src/components/panelAdmin/CoursesPA.jsx
@@ -1,55 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar";
+import Select from "react-select";
 
 function CoursesPA() {
+  const [selectedOptions, setSelectedOptions] = useState();
+
+  const optionList = []?.map((video) => {
+    // return {
+    //   value: video._id,
+    //   label: video.name,
+    // };
+  });
+
+  function handleSelect(data) {
+    setSelectedOptions(data);
+  }
   return (
     <div className="text-2x1 font-semibold flex h-screen">
       <Sidebar />
       <div className="w-full h-full" style={{ backgroundColor: "#C9C4B8" }}>
-        <div class="h-screen">
+        <div className="h-screen">
           <form
-            class="w-full max-w-xs bg-white flex flex-col py-5 px-8 rounded-lg shadow-lg"
+            className="w-full max-w-xs bg-white flex flex-col py-5 px-8 rounded-lg shadow-lg"
             action=""
           >
-            <label class="text-gray-700 font-bold py-2" for="">
-              Username
+            <h2 className="text-gray-700 font-bold py-2 text-center text-xl">
+              Create Courses
+            </h2>
+            <label className="text-gray-700 font-bold py-2" for="">
+              Name
             </label>
             <input
-              class="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3"
+              className="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3"
               type="text"
-              placeholder="Username"
+              placeholder="Name"
             />
-            <label class="text-gray-700 font-bold py-2" for="">
-              Password
+            <label className="text-gray-700 font-bold py-2" for="">
+              Description
+            </label>
+            <textarea
+              className="text-gray-700 shadow border rounded border-gray-300 mb-3 py-1 px-3 focus:outline-none focus:shadow-outline"
+              placeholder="Description"
+            />
+            <label className="text-gray-700 font-bold py-2" for="">
+              Image
             </label>
             <input
-              class="text-gray-700 shadow border rounded border-gray-300 mb-3 py-1 px-3 focus:outline-none focus:shadow-outline"
-              type="password"
-              placeholder="********"
-            />
-            <label class="text-gray-700 font-bold py-2" for="">
-              Username
-            </label>
-            <input
-              class="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3"
+              className="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3"
               type="text"
-              placeholder="Username"
+              placeholder="Image"
             />
-            <label class="text-gray-700 font-bold py-2" for="">
-              Username
+            <label className="text-gray-700 font-bold py-2" for="">
+              Videos
             </label>
-            <input
-              class="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3"
-              type="text"
-              placeholder="Username"
+            <Select
+              options={optionList}
+              placeholder="Videos"
+              value={selectedOptions}
+              onChange={handleSelect}
+              isSearchable={true}
             />
-            <div class="flex justify-between items-center my-4">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4">
-                Sign In
+            <div className="flex justify-end items-center my-4 mt-10">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 ">
+                Create
               </button>
-              <a class="text-blue-600 hover:text-blue-800 font-bold" href="#">
-                Forgot Password?
-              </a>
             </div>
           </form>
         </div>
