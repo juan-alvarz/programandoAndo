ce16553cb50fd9bc78f15c63b44926db1196edfc
juan-alvarz <juanalvarez0925@gmail.com>
2022-09-08 13:32:35 -0500

    validators added

--- api/validators/users.js
+++ api/validators/users.js
@@ -7,7 +7,7 @@ const validatorCreateUser = [
   check("email").exists().notEmpty().isEmail(),
 
   check("password").exists().notEmpty().isLength({ min: 8, max: 16 }),
-
+  
   (req, res, next) => {
     return validateResults(req, res, next);
   },
