8c39f7104f08e0a6305511d3b51f02d4ea6ef211
juan-alvarz <juanalvarez0925@gmail.com>
2022-09-09 14:17:24 -0500

    foro updated

--- api/controllers/foro.js
+++ api/controllers/foro.js
@@ -10,13 +10,10 @@ const getForos = async (req, res) => {
         populate: {
           path: "authorComment",
           select: "name",
-<<<<<<< HEAD
-=======
           // path: "answers",
           // populate: {
           //     path: "authorComment"
           // }
->>>>>>> Roge
         },
       })
       .populate({
@@ -28,13 +25,8 @@ const getForos = async (req, res) => {
             select: "name",
           },
         },
-<<<<<<< HEAD
-      });
-    return res.status(200).json(data);
-=======
       }); // .populate("idVideo")
     res.status(200).json(data);
->>>>>>> Roge
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, msg: err.message });
@@ -65,23 +57,12 @@ const getForoById = async (req, res) => {
               select: "name",
             },
           },
-<<<<<<< HEAD
-        });
-      // if (!notificationId) {
-      //   res.send({ message: `The foro with the: ${id} does not exist` });
-      // } else {
-      //   res.status(200).send(notificationId);
-      // }
-      console.log(notificationId);
-      return res.status(200).send(notificationId);
-=======
         }); //.populate("idVideo")
       if (!notificationId) {
         res.send({ message: `The foro with the: ${id} does not exist` });
       } else {
         res.status(200).send(notificationId);
       }
->>>>>>> Roge
     }
   } catch (error) {
     handleHttpError(res, "ERROR_GET_FORO");
@@ -89,34 +70,22 @@ const getForoById = async (req, res) => {
 };
 
 const createForo = async (req, res) => {
-<<<<<<< HEAD
-  const { comments, idVideo } = req.body;
-  try {
-    const creado = await foroModel.create({
-      comments,
-      idVideo,
-=======
   const { comments } = req.body;
   try {
     const creado = await foroModel.create({
       comments,
->>>>>>> Roge
     });
     res.status(200).send(creado);
   } catch (error) {
     res.send({ msg: "The Foro Could not be created" });
   }
 };
-<<<<<<< HEAD
-
-=======
 /* 
   {
     "authorComment": "63127d65452e091676932e21",
     "content": "asdfsafsafasfasdfsa"
   }
   */
->>>>>>> Roge
 const updateForo = async (req, res) => {
   const { id } = req.params;
   const { commentId, ...body } = req.body;
@@ -134,7 +103,6 @@ const updateForo = async (req, res) => {
   } catch (error) {
     res.status(404).json(error.message);
   }
-<<<<<<< HEAD
 };
 
 const softDeleteForo = async (req, res) => {
@@ -158,31 +126,6 @@ const restoreForo = async (req, res) => {
   }
 };
 
-=======
-};
-
-const softDeleteForo = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deleted = await foroModel.delete({ _id: id });
-    res.status(200).send(deleted);
-  } catch (error) {
-    res.json(error.message);
-  }
-};
-
-// PATCH!!
-const restoreForo = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const restored = await foroModel.restore({ _id: id });
-    res.status(200).send(restored);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-};
-
->>>>>>> Roge
 module.exports = {
   getForos,
   createForo,
