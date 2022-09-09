9fe43a9ddcc1f2c7c669b482505b092a302960e5
juan-alvarz <juanalvarez0925@gmail.com>
2022-09-09 13:43:12 -0500

    donations done¿

--- client/src/components/CancelDonation.jsx
+++ client/src/components/CancelDonation.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import NavBar from "./NavBar";
 
 export default function CancelDonation() {
   return (
     <div>
+      <NavBar />
       <h1>Cancel donation</h1>
       <span>You are canceled donation in course</span>
     </div>
