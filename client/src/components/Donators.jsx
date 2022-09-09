12e1ed4f720df28b0e77db35aec1b17de515df54
juan-alvarz <juanalvarez0925@gmail.com>
2022-09-09 15:38:41 -0500

    donators broken, kllmepls

--- client/src/components/Donators.jsx
+++ client/src/components/Donators.jsx
@@ -38,22 +38,63 @@ function Donators() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h2>Donation</h2>
-          <label>Amount: </label>
+    /* <div>
+      <div className="w-full h-full" style={{ backgroundColor: "#C9C4B8" }}>
+        <div className="h-screen">
+          <form
+            className="w-full max-w-xs bg-white flex flex-col py-5 px-8 rounded-lg shadow-lg"
+            action=""
+          >
+            <h2 className="text-gray-700 font-bold py-2 text-center text-xl">
+              Donation
+            </h2>
+            <label className="text-gray-700 font-bold py-2" for=""></label>
+            <input
+              className="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3"
+              type="number"
+              placeholder="Donation $ 0.000"
+            />
+
+            <div className="flex justify-center items-center my-4 mt-10">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 ">
+                <a href="#" target="_blank">
+                  Done
+                </a>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div> */
+    <div className="w-full h-full" style={{ backgroundColor: "#C9C4B8" }}>
+      <div className="h-screen">
+        <form
+          className="w-full max-w-xs bg-white flex flex-col py-5 px-8 rounded-lg shadow-lg"
+          onSubmit={handleSubmit}
+          style={{ margin: "auto" }}
+        >
+          <h2 className="text-gray-700 font-bold py-2 text-center text-xl">
+            Donation
+          </h2>
+          <label className="text-gray-700 text-center font-bold py-2">
+            Amount
+          </label>
           <input
+            className="text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3"
             type="number"
             value={amount}
             onChange={(e) => handleChange(e)}
             min="0"
           />
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+          <button
+            type="submit"
+            /* className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" */
+            style={{ margin: "auto" }}
+          >
+            Donate
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
