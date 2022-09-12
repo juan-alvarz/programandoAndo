1deae2ce233c57b9272a4f85137c6198b2ca4ba0
frangiuliano <francosebastiangiuliano@gmail.com>
2022-09-12 01:09:44 -0300

    NavBar and Bell notification

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -5,12 +5,12 @@ export const CourseDetail = ({ element }) => {
   let el = element;
 
   return (
-    <div className="grid justify-items-center mt-4  bg-gray-200 pt-5 border-t-4">
+    <div className=" mt-4 bg-gray-200 pt-5" style={{backgroundColor: 'rgb(198, 198, 198)'}}>
         <div>
            <img style={{maxHeight:100,maxWidth:150}} className="mb-5" src={el.image}></img> 
         </div>
         <h2 style={{color: 'rgb(17, 52, 82)'}} className="text-lg font-bold mb-2 text-gray-700">{el.name}</h2>
-        <div className="flex mb-5 w-96 justify-center text-center font-medium text-sm">
+        <div className="flex mb-5 font-medium text-sm">
             {el.description}
         </div>
    </div>
--- client/src/components/NavBarAdmin.jsx
+++ client/src/components/NavBarAdmin.jsx
@@ -191,7 +191,7 @@ const handlelogout = (e) => {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <Menu.Item>
+                            {/* <Menu.Item>
                               {({ active }) => (
                                 <a
                                   href="#"
@@ -200,7 +200,7 @@ const handlelogout = (e) => {
                                   Your Profile admin
                                 </a>
                               )}
-                            </Menu.Item>
+                            </Menu.Item> */}
                             <Menu.Item>
                               {({ active }) => (
                                 <NavLink
