dced594baacaf42f1109ca89e804e504d7e583f0
Danzsv <danzsv94@gmail.com>
2022-09-15 09:51:45 -0500

    github ready

--- client/src/components/GitHub.jsx
+++ client/src/components/GitHub.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { GITHUB, gitHubRedirectURL, path } from "../utils/gitHubCredentials.js";
-import { useEffect, useState } from "react";
-import jwt_decode from "jwt-decode";
-import { createsUser, googleUserLogin } from "../redux/actions";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import github from "../utils/images/4e309bf2-8d38-11e5-8d46-b347b2bd242e.png";
 
-export default function GitHub() {
-  const handleClick = () => {};
+export default function GitHub() { 
   return (
     <div>
       <a
