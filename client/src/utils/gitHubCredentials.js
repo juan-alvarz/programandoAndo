ae231c788aa6fdea6f954845e0c570bd144f5a6c
Luis Blanco <luiswblancod@gmail.com>
2022-09-20 22:14:10 -0500

    cambios viejos

---
+++ client/src/utils/gitHubCredentials.js
@@ -0,0 +1,14 @@
+const GITHUB =
+  "1b495944bfc018b1438e";
+const GITHUB_SECRET =
+  "4409954a9051068223e20aa99fe14f6ea795c8f3";
+const path = "/"
+
+const gitHubRedirectURL = "http://localhost:3001/api/auth/github_login";
+
+module.exports = {
+  GITHUB,
+  GITHUB_SECRET,
+  path, 
+  gitHubRedirectURL 
+};
